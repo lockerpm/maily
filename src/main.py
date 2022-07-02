754b02c1cb547b267726a16fdf23e95da26eac60
trungnh <trungnh@cystack.net>
2022-07-02 15:13:09 +0700

    add functions

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ def add_destination(email):
     url = f'{CF_ROOT_API}/accounts/{CF_ACCOUNT_ID}/email/routing/addresses'
     payload = {'email': email}
     res = requests.post(url, json=payload, headers=HEADERS)
-    print(res.json())
     return res.json()
 
 
@@ -27,11 +26,28 @@ def add_mask(mask, destination):
         }]
     }
     res = requests.post(url, json=payload, headers=HEADERS)
-    print(res.json())
+    return res.json()
+
+
+def get_destinations():
+    url = f'{CF_ROOT_API}/accounts/{CF_ACCOUNT_ID}/email/routing/addresses'
+    page = 1
+    destinations = list()
+    while True:
+        params = {'page': page}
+        r = requests.get(url, headers=HEADERS, params=params)
+        result = r.json()['result']
+        if len(result) == 0:
+            break
+        else:
+            page += 1
+            destinations.extend(result)
+    return destinations
 
 
 if __name__ == '__main__':
     # add_destination('me@trungnh.com')
     # for i in range(1000):
     #     add_destination(f'{token_hex(6)}@trungnh.com')
-    add_mask(token_hex(6), 'me@trungnh.com')
+    # add_mask(token_hex(6), 'me@trungnh.com')
+    get_destinations()
