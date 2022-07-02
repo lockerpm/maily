7a52218101ac9976ef15f2999dd972ea6cc747ec
trungnh <trungnh@cystack.net>
2022-07-02 15:28:29 +0700

    add functions

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ def get_destinations():
     page = 1
     destinations = list()
     while True:
-        params = {'page': page}
+        params = {'page': page, 'per_page': 50}
         r = requests.get(url, headers=HEADERS, params=params)
         result = r.json()['result']
         if len(result) == 0:
@@ -45,9 +45,45 @@ def get_destinations():
     return destinations
 
 
+def get_destination_tag(email):
+    url = f'{CF_ROOT_API}/accounts/{CF_ACCOUNT_ID}/email/routing/addresses'
+    page = 1
+    while True:
+        params = {'page': page, 'per_page': 50}
+        r = requests.get(url, headers=HEADERS, params=params)
+        result = r.json()['result']
+        for r in result:
+            if r['email'] == email:
+                return r['tag']
+        if len(result) == 0:
+            return None
+        else:
+            page += 1
+
+
+def delete_destination(email):
+    tag = get_destination_tag(email)
+    url = f'{CF_ROOT_API}/accounts/{CF_ACCOUNT_ID}/email/routing/addresses/{tag}'
+    r = requests.delete(url, headers=HEADERS)
+    return r.json()
+
+
+def delete_all_destinations():
+    destinations = get_destinations()
+    for d in destinations:
+        url = f'{CF_ROOT_API}/accounts/{CF_ACCOUNT_ID}/email/routing/addresses/{d["tag"]}'
+        r = requests.delete(url, headers=HEADERS)
+        print(f'Deleting destination {d["email"]}')
+    return True
+
+
 if __name__ == '__main__':
     # add_destination('me@trungnh.com')
     # for i in range(1000):
     #     add_destination(f'{token_hex(6)}@trungnh.com')
     # add_mask(token_hex(6), 'me@trungnh.com')
-    get_destinations()
+    # get_destinations()
+    # x = get_destination_tag('me@trungnh.com')
+    # x = delete_destination('107@trungnh.com')
+    # print(x)
+    delete_all_destinations()
